33d290c937a441393f1adb126795ad810aedbd82
Min <125743623+min-04A@users.noreply.github.com>
2025-04-20 16:05:25 -0400

    second commit: Add Variables

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,3 +1,21 @@
-public class EnigmaFrame {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EnigmaFrame extends JFrame
+{
+    private JComboBox<Integer> innerRotor;
+    private JComboBox<Integer> midRotor;
+    private JComboBox<Integer> outerRotor;
+    private JTextField initialPosition;
+    private JTextArea inArea;
+    private JTextArea outArea;
+    private JButton encryptButton;
+    private JButton decryptButton;
+
+    public EnigmaFrame()
+    {
+
+
+    }
 }
