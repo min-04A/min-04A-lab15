d301fccc07caace85a8097ec61e810e8c6c0a480
Min <125743623+min-04A@users.noreply.github.com>
2025-04-20 18:40:28 -0400

    fourth commit: Add top panel

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -20,5 +20,31 @@ public class EnigmaFrame extends JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // available to close window
         setLayout(new BorderLayout()); // set layout of panel
 
+        // Create top panel with flow layout and add GUI elements
+        JPanel topPanel = new JPanel(new FlowLayout()); 
+
+        // Need getRotorNumbers to make users to choose three rotors
+        innerRotor = new JComboBox<>(getRotorNumbers());
+        midRotor = new JComboBox<>(getRotorNumbers());
+        outerRotor = new JComboBox<>(getRotorNumbers());
+
+        // Make a space for users to type three letters to start with
+        initialPosition = new JTextField(3);
+
+        // Add elements
+        topPanel.add(new JLabel("Inner"));
+        topPanel.add(innerRotor);
+        topPanel.add(new JLabel("Middle"));
+        topPanel.add(midRotor);
+        topPanel.add(new JLabel("Out"));
+        topPanel.add(outerRotor);
+        topPanel.add(new JLabel("Initial Positions"));
+        topPanel.add(initialPosition);
+    }
+
+    // Give 5 numbers for users to choose
+    private Integer[] getRotorNumbers() 
+    {
+        return new Integer[]{1, 2, 3, 4, 5};
     }
 }
